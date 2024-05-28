7a73d511dbe925872303da11c4dd0c49b3c5441f
Matias Ludueña <matiasluduena23@gmail.com>
2024-05-28 19:55:03 -0300

    mercadopago backend

--- app/components/DialogPay.tsx
+++ app/components/DialogPay.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
 	Dialog,
@@ -8,9 +10,26 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
-import { Dispatch, SetStateAction } from 'react';
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { mercadopayment } from '../lib/actions';
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import { useEffect } from 'react';
 
 export function DialogPay() {
+	useEffect(() => {
+		initMercadoPago(process.env.MERCADO_PUBLIC_KEY!, {
+			locale: 'es-AR',
+		});
+	}, []);
+
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
@@ -21,10 +40,36 @@ export function DialogPay() {
 					<DialogTitle>Comprar consultas</DialogTitle>
 					<DialogDescription>comprar mas consultas</DialogDescription>
 				</DialogHeader>
+				<form action={mercadopayment} className="flex flex-col gap-8">
+					<Select name="cantidad" required>
+						<SelectTrigger className="w-full">
+							<SelectValue placeholder="Selecciona una cantidad" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectGroup>
+								<SelectItem value="1000">
+									$1000 - 30 consultas
+								</SelectItem>
+								<SelectItem value="2000">
+									$2000 - 60 consultas
+								</SelectItem>
+								<SelectItem value="5000">
+									$5000 - 200 consultas
+								</SelectItem>
+							</SelectGroup>
+						</SelectContent>
+					</Select>
 
-				<DialogFooter>
-					<Button type="submit">Mercado Pago</Button>
-				</DialogFooter>
+					<Button type="submit">Ir a mercadopago</Button>
+					<div id="wallet_container"></div>
+				</form>
+
+				<Wallet
+					initialization={{ preferenceId: 'consultas' }}
+					customization={{ texts: { valueProp: 'smart_option' } }}
+				/>
+
+				<DialogFooter></DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { revalidatePath } from 'next/cache';
 import prisma from './db';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
 
 export async function updateUser(user: User) {
 	try {
@@ -21,4 +22,32 @@ export async function updateUser(user: User) {
 	} finally {
 		revalidatePath('/chat');
 	}
+}
+
+// SDK de Mercado Pago
+// Agrega credenciales
+const client = new MercadoPagoConfig({
+	accessToken: process.env.MERCADO_ACCESS_TOKEN!,
+});
+
+export async function mercadopayment(formData: FormData) {
+	const preference = new Preference(client);
+
+	const cantidad = Number(formData.get('cantidad'));
+
+	preference
+		.create({
+			body: {
+				items: [
+					{
+						title: 'compra consultas',
+						quantity: 1,
+						unit_price: cantidad,
+						id: cantidad.toString(),
+					},
+				],
+			},
+		})
+
+		.then((data) => data.id);
 }
